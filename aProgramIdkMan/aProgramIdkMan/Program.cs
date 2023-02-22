e40d18c043edd0b117421c489bd38b3010133cd3
Eliza Voets <25413@ma-web.nl>
2023-02-22 22:14:02 +0100

    first lesson

---
+++ aProgramIdkMan/aProgramIdkMan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace aProgramIdkMan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Man i almost handed this in too late :sob:");
+        }
+    }
+}
