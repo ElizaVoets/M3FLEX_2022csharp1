8cebbd5c14ef41f5defa44cff68bae13b3dbf0ff
Eliza Voets <25413@ma-web.nl>
2023-03-10 10:15:02 +0100

    les 3

---
+++ les 3/IfElseQuiz/IfElseQuiz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace IfElseQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string amogus = "sus";
+
+            if(259 + 407 == 666 && (44 < 39 || 80 != 60))
+            {
+                Console.WriteLine("HAIL SATAN");
+            }
+        }
+    }
+}
---
+++ les 3/IfOpdracht4/IfOpdracht4/EnemyCollider.cs
@@ -0,0 +1,21 @@
+﻿namespace IfOpdracht4
+{
+    internal class EnemyCollider
+    {
+        internal bool CanPassWall(Enemy enemy)
+        {
+            return (enemy.incoporial || enemy.flying || enemy.subterranian);
+            
+            //gebruik hier een || (OR)
+			//je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+            //vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian) true is;
+        }
+        internal bool CanPassWater(Enemy enemy)
+        {
+            return (enemy.flying|| enemy.subterranian || enemy.incoporial || enemy.swimmer);
+            //gebruik hier een || (OR)
+			//je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+            //vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian,swimmer) true is;
+        }
+    }
+}
