b8dfb19ca1759c2ccbef678c5baa5984b27c0ff5
Eliza Voets <25413@ma-web.nl>
2023-03-31 10:26:56 +0200

    AAAAAAAAAAAAAA

---
+++ les 6/PropertyOpdracht1/PropertyOpdracht1/Persoon.cs
@@ -0,0 +1,12 @@
+﻿namespace PropertyOpdracht1
+{
+    internal class Persoon
+    {
+        public Persoon()
+        {
+        }
+
+        public string Name { get; internal set; }
+        public int Age { get; internal set; }
+    }
+}
